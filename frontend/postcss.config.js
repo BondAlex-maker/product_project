7868023bf96ff51c3b11bd558e934bad37c8f161
alex <bondareko.alex.sergeevich@gmail.com>
2025-10-19 14:32:53 +0200

    fix: fix tailiwind styles

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,8 @@
-export default {
-    plugins: {
-        '@tailwindcss/postcss': {},
-        autoprefixer: {},
-    },
-}
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+    ],
+})
